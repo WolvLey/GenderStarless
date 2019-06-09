01470df405b6951f7ca8189e4f7f5929a6a1c62f
WolvLey <30508429+WolvLey@users.noreply.github.com>
2019-06-10 00:34:18 +0200

    implementing single removal

--- content.js
+++ content.js
@@ -1,5 +1,14 @@
 const regEx = /(?!\w_+)(_|\*)(innen|in|e)/gi
 
+jQuery.extend(
+    jQuery.expr[':'], {
+        regex: function(a, i, m, r) {
+            var r = regEx;
+            return r.test(jQuery(a).text());
+        }
+    }
+);
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message === 'clicked_browser_action') {
 		removeGenderStar()
@@ -7,5 +16,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 })
 
 function removeGenderStar() {
-    console.log($('p'))
+	$(`p:regex("${regEx}")`).each((i, el)=>{
+		console.log($(el).text((i, old)=>{
+			return old.replace(regEx, '');
+		}))
+
+	});
 }
