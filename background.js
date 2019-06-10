b006156468e607ebc9a296223b9243d5b7ca4a3a
WolvLey <30508429+WolvLey@users.noreply.github.com>
2019-06-11 00:20:21 +0200

    Badge implementation

--- background.js
+++ background.js
@@ -1,7 +1,35 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-    });
-  });
-  
+	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+		var activeTab = tabs[0]
+		chrome.tabs.sendMessage(
+			tabId,
+			{
+				message: 'clicked_browser_action',
+			},
+			function(response) {
+				chrome.browserAction.setBadgeText({ text: '0' })
+			},
+		)
+	})
+})
+
+// chrome.tabs.onActivated.addListener(function(activeInfo) {
+//   console.log(activeInfo.tabId);
+// });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+		var activeTab = tabs[0]
+		chrome.tabs.sendMessage(
+			tabId,
+			{ message: 'update_browser_action' },
+			function(response) {
+				if (!response) return
+				if (!response.hasOwnProperty('len')) return
+
+				let len = response.len ? response.len : 0
+				chrome.browserAction.setBadgeText({ text: len.toString() })
+			},
+		)
+	})
+})
--- content.js
+++ content.js
@@ -1,25 +1,35 @@
 const regEx = /(?!\w_+)(_|\*)(innen|in|e)/gi
 
-jQuery.extend(
-    jQuery.expr[':'], {
-        regex: function(a, i, m, r) {
-            return regEx.test(jQuery(a).text());
-        }
-    }
-);
+jQuery.extend(jQuery.expr[':'], {
+	regex: function(a, i, m, r) {
+		return regEx.test(jQuery(a).text())
+	},
+})
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.message === 'clicked_browser_action') {
-		removeGenderStar()
+	switch (request.message) {
+		case 'clicked_browser_action':
+			removeGenderStar()
+			sendResponse()
+			break
+		case 'update_browser_action':
+			sendResponse({ len: countOccurrence() })
+			break
+		default:
+			break
 	}
 })
 
+function countOccurrence() {
+	return $(`p:regex("${regEx}")`).length
+}
+
 function removeGenderStar() {
-	$(`p:regex("${regEx}")`).each((i, el)=>{
-		let  target = $(el).text((i, old)=>{
-			return old.replace(regEx, '');
+	$(`p:regex("${regEx}")`).each((i, el) => {
+		let target = $(el).text((i, old) => {
+			return old.replace(regEx, '')
 		})
 
 		console.log(target)
-	});
+	})
 }
